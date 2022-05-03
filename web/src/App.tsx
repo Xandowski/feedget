3d3600ed4a91dd6db84e98c0cfbc438de053da31
xandowski <xandy-moraes@hotmail.com>
2022-05-03 19:17:36 -0300

    config tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
+import { Nav } from "./components/Nav"
 import { Widget } from "./components/Widget"
 
 export const App = () =>{
   return (
     <>
+      <Nav/>
       <Widget/>
     </>
   )
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,53 @@
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  darkMode: 'class',
+  content: ["./src/**/*.tsx", "./*.html"],
   theme: {
     extend: {
       colors: {
         brand: {
-          500: '#8257E5'
-        }
+          surface: '#8257E5',
+          hover: '#996DFF'
+        },
+        dark: {
+          surface: {
+            primary: '#18181B',
+            secondary: {
+              main: '#27272A',
+              hover: '#3F3F46'
+            },
+            stroke: '#52525B',
+            tooltip: '#F4F4F5',
+            text: {
+              primary: '#F4F4F5',
+              secondary: '#A1A1AA',
+              onToolTip: '#27272A'
+            },
+            background: '#09090A'
+          }
+        },
+        light: {
+          surface: {
+            primary: '#FFFFFF',
+            secondary: {
+              main: '#F4F4F5',
+              hover: '#E4E4E7'
+            },
+            stroke: '#D4D4D8',
+            tooltip: '#27272A',
+            text: {
+              primary: '#27272A',
+              secondary: '#71717A',
+              onToolTip: '#F4F4F5'
+            },
+            background: '#FFFFFF'
+          }
+        },
+      },
+      width: {
+        '84': '21rem'
+      },
+      height: {
+        '18': '4.5rem'
       }
     },
   },
