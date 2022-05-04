1b41bc618442ef36464bf88f19a4a3b863f1c2d1
xandowski <xandy-moraes@hotmail.com>
2022-05-03 23:54:32 -0300

    feat: create Footer component

---
+++ web/src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center">
+      <p className="text-sm dark:text-dark-surface-text-secondary">Feito com pela Rocketseat</p>
+    </footer>
+  )
+}
