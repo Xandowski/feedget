a8de1202e03b00ad8496348f48ed6f82fa6766ba
xandowski <xandy-moraes@hotmail.com>
2022-05-04 23:29:22 -0300

    fix: text footer

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="flex justify-center">
-      <p className="text-sm dark:text-dark-surface-text-secondary">Feito com pela Rocketseat</p>
+      <p className="text-sm dark:text-dark-surface-text-secondary">Feito com <span className="text-brand-surface"> ♥ </span> pela Rocketseat</p>
     </footer>
   )
 }
