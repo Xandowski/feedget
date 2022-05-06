f673f594e912964d0398ed7d0687e3f4a46d986e
xandowski <xandy-moraes@hotmail.com>
2022-05-06 19:51:05 -0300

    fix: api url

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -23,7 +23,7 @@ export const FeedbackContentStep = ({
     
     setIsSendingFeedback(true)
 
-    await fetch(`${import.meta.env.VITE_API_URL}/feedback`, {
+    await fetch(`${import.meta.env.VITE_API_URL}feedback`, {
       method: 'POST',
       mode: 'cors',
       headers: {
