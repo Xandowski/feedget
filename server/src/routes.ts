21f69340b1f21c762f8e897e06edfd887583c25c
xandowski <xandy-moraes@hotmail.com>
2022-05-06 20:47:00 -0300

    fix: remove try catch block

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,10 +8,10 @@ export const routes = Router()
 routes.post('/feedback', async (req, res) => {
   const { type, comment, screenshot } = req.body
 
- try {
+ 
   const prismaFeedbackRepository = new PrismaFeedbackRepository()
   const nodemailerMailAdapter = new NodemailerMailAdapter()
-  
+
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
     prismaFeedbackRepository,
     nodemailerMailAdapter
@@ -24,8 +24,4 @@ routes.post('/feedback', async (req, res) => {
   })
 
   return res.sendStatus(201).send()
- } catch (err) {
-   console.log(err)
-  return res.sendStatus(201).send()
- }
 })
