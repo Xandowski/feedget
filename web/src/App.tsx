2af46b6515b2814f0c635c78bda06cbad4e37ab1
xandowski <xandy-moraes@hotmail.com>
2022-05-08 15:29:05 -0300

    config auth0 provider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
+import { Auth0Provider } from "@auth0/auth0-react"
 import { Index } from "./pages"
 
 export const App = () =>{
   return (
-    <Index/>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      redirectUri={import.meta.env.VITE_REDIRECT_URL}
+    >
+      <Index/>
+    </Auth0Provider>
   )
 }
 
