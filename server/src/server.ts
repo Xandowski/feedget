0dc7066201a00106a2901b11034a48edf5af2f50
xandowski <xandy-moraes@hotmail.com>
2022-05-08 15:22:06 -0300

    fix: server size limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import BodyParser from 'body-parser'
 import cors from 'cors'
 import express, { json } from "express"
 import { routes } from './routes'
@@ -5,7 +6,8 @@ import { routes } from './routes'
 export const app = express()
 
 app.use(cors())
-app.use(json())
+app.use(json({limit: '50mb'}))
+app.use(BodyParser.urlencoded({limit: '50mb', extended: true }))
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
