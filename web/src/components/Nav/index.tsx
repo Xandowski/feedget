836d4b5549526acb736c402d3bd69caea7b43f2e
xandowski <xandy-moraes@hotmail.com>
2022-05-08 16:02:06 -0300

    fix: load user picture only autehnticated

--- web/src/components/Nav/index.tsx
+++ web/src/components/Nav/index.tsx
@@ -1,23 +1,28 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import Logo from '../../assets/imgs/logo.svg';
 
-
 const LogoutButton = ({}) => {
-  const { user, logout } = useAuth0()
+  const { user, logout, isLoading, isAuthenticated } = useAuth0()
 
+  if (isAuthenticated){
   return (
     <div className="flex flex-col justify-center items-center">
       <div>
-        <img className="w-10 h-10 rounded-full" src={user?.picture} alt="foto do usuário" />
+        {isLoading ? (
+          <div className="w-10 h-10 rounded-full bg-light-surface-text-secondary"></div>
+        ): (
+          <img className="w-10 h-10 rounded-full" src={user?.picture} alt="foto do usuário" />
+        )}
+        
       </div>
       <button className="text-sm text-light-surface-text-secondary dark:text-dark-surface-text-secondary" onClick={() => logout({ returnTo: window.location.origin })}>
         Sair
       </button>
     </div>
-  )
+  )}
+
+  return null
 }
-
-
 
 export const Nav = () => {
   return (
