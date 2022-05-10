67da472bfaba609a70dcbffcc0e3356dedf921ad
xandowski <xandy-moraes@hotmail.com>
2022-05-10 18:36:48 -0300

    fix: cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,14 +5,19 @@ import { routes } from './routes'
 
 export const app = express()
 
-app.use(cors({
-  "origin": /feedget-xandowski.vercel.app/,
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "credentials": true,
-  "allowedHeaders": ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With', 'Origin', 'Accept']
-}))
+app.use(cors())
 app.use(json({limit: '50mb'}))
 app.use(BodyParser.urlencoded({limit: '50mb', extended: true }))
+app.use((req, res, next) => {
+  res.header('Acess-control-Allow-Origin', 'https://feedget-xandowski.vercel.app')
+  res.header('Acess-control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+  if (req.method === 'OPTIONS') {
+    res.header('Acess-control-Allow-Headers', 'GET, POST, PUT, PATCH, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+})
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
